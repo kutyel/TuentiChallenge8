3cf51b357f129bc5a06f0c2cca04b6d11ccd7b29
kutyel <flaviocorpa@gmail.com>
2018-04-28 15:14:19 +0200

    Small refactor

--- challenge3/script.js
+++ challenge3/script.js
@@ -1,6 +1,6 @@
 import { createReadStream, createWriteStream } from 'fs'
 import { createStream } from 'byline'
-import { compose, map, split, toString, trim, uniq } from 'ramda'
+import { compose, map, reduce, split, toString, trim, uniq } from 'ramda'
 
 const read = createReadStream('./submitInput.txt')
 const write = createWriteStream('./submitOutput.txt')
@@ -9,21 +9,18 @@ const stream = createStream(read)
 // EXAMPLES:
 // MA -> A, B, C#, D, E, F#, G#, A
 // MA# -> A#, C, D, D#, F, G, A, A#
-const allKeys =
+const keys =
   'MA MA# MB MC MC# MD MD# ME MF MF# MG MG# mA mA# mB mC mC# mD mD# mE mF mF# mG mG#'
-const tones =
-  'C C# D D# E F F# G G# A A# B C C# D D# E F F# G G# A A# B C C# D D# E F F# G G# A A# B'
-const allTones = tones.split(' ')
+const tones = 'C C# D D# E F F# G G# A A# B C C# D D# E F F# G G# A A# B'
+const stones = split(' ', tones)
 const M = [0, 2, 4, 5, 7, 9, 11]
 const m = [0, 2, 3, 5, 7, 8, 10]
-const to = note => tone => allTones[allTones.indexOf(note) + tone]
-const genScale = e => {
-  const note = e.slice(1)
-  return e[0] === 'M' ? M.map(to(note)) : m.map(to(note))
-}
-const scales = allKeys
-  .split(' ')
-  .reduce((obj, esc) => ({ ...obj, [esc]: genScale(esc) }), {})
+const to = note => tone => stones[stones.indexOf(note) + tone]
+const genScale = (e, note = e.slice(1)) => map(to(note))(e[0] === 'M' ? M : m)
+const scales = compose(
+  reduce((o, e) => ({ ...o, [e]: genScale(e) }), {}),
+  split(' ')
+)(keys)
 const eqs = {
   Ab: 'G#',
   Bb: 'A#',
@@ -44,15 +41,15 @@ stream.on('data', str => {
   if (line > 0) {
     const numNotes = Number(str)
     if (numNotes === 0) {
-      write.write(`Case #${test}: ${allKeys}\n`)
+      write.write(`Case #${test}: ${keys}\n`)
       test++
     } else if (isNaN(numNotes)) {
       const notes = compose(uniq, map(toSharp), split(' '), trim, toString)(str)
-      const result = Object.entries(scales).reduce(
-        (scales, [key, values]) =>
-          notes.every(n => values.includes(n)) ? scales + key + ' ' : scales,
+      const result = reduce(
+        (keys, [k, val]) =>
+          notes.every(n => val.includes(n)) ? keys + k + ' ' : keys,
         ''
-      )
+      )(Object.entries(scales))
       write.write(`Case #${test}: ${trim(result) || 'None'}\n`)
       test++
     }
